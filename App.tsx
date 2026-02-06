424fe2a84489d16d639858587d81cd7049c53e02
Emmanuel Bamigbade <emmanuel.bamigbade@lexisnexis.com>
2026-02-06 00:54:28 +0000

    feat: wrap HomeScreen in SafeAreaProvider and update dependencies in package.json

--- App.tsx
+++ App.tsx
@@ -1,5 +1,10 @@
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
-  return <HomeScreen />;
+  return (
+    <SafeAreaProvider>
+      <HomeScreen />
+    </SafeAreaProvider>
+  );
 }
